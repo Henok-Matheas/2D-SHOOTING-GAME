bc465c4ceec6f8556583f96b7e3828e42a893aa4
zerihunttt655674 <zeerin4@gmail.com>
2022-06-12 05:02:27 +0300

    map

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
 import os
 import pygame
 from pyparsing import col
-
-class Wall(pygame.sprite.Sprite):
-    def __init__(self, width, height, x, y, color):
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-        
-        self.rect.topleft = [x,y]
-
-
+from gameWalls import walls
 
 WHITE = (255,255,255)
 WIDTH, HEIGHT = 900, 500
+WALL_THICKNESS = 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Nigga")
-
-wall = Wall(600, 50, 0, 200, (255,255,255))
-wall2 = Wall(50, 200, wall.rect.width-50, wall.rect.y, WHITE)
-walls = pygame.sprite.Group()
-walls.add(wall)
-walls.add(wall2)
 
 
 
